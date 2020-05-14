63ca9567706cc1a0ff538662ad51ff9ad64c43fe
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-05-14 18:11:00 -0500

    [update]: add function mul

--- get_op.c
+++ get_op.c
@@ -20,6 +20,7 @@ void get_op(stack_t **stack, unsigned int line_number)
 				{"add", add},
 				{"sub", sub},
 				{"div", div_f},
+				{"mul", mul_f},
 				{NULL, NULL}
 	};
 	for (count = 0; op_codes[count].opcode != NULL; count++)

955a6cf5072eb5e24e33e5b36341b93192fc27ac
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-05-14 16:57:55 -0500

    [update]: add function swap

--- monty.h
+++ monty.h
@@ -67,5 +67,6 @@ void pall(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void pop_f(stack_t **head, unsigned int line_number);
 void pint_f(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
 
 #endif

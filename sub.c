b12e3d5b4b20185a0f04e3a28a7d03e3a7b378ad
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-05-14 17:54:41 -0500

    [update]: add function div

--- sub.c
+++ sub.c
@@ -28,3 +28,38 @@ void sub(stack_t **stack, unsigned int line_number)
 	pop_f(stack, line_number);
 	(*stack)->n = sum;
 }
+
+/**
+ * div - this function div two nodes
+ * @stack: double list reversed
+ * @line_number: line of file
+ * Return:
+ */
+void div(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+	int sum, count = 0;
+
+	if ((*stack)->n == 0)
+	{
+		dprintf(STDERR_FILENO, "L%u: division by zero\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+	tmp = *stack;
+	while (tmp)
+	{
+		tmp = tmp->next;
+		count++;
+	}
+	if (count < 2)
+	{
+		dprintf(STDERR_FILENO, "L%u: can't div, stack too short\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+	sum = (*stack)->next->n;
+	sum -= (*stack)->n;
+	pop_f(stack, line_number);
+	(*stack)->n = sum;
+}

0ef24a056b852e91c16d014be882d6a6c3ab329d
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-05-14 19:28:52 -0500

    [update] add function pchar

--- sub.c
+++ sub.c
@@ -129,3 +129,28 @@ void module(stack_t **stack, unsigned int line_number)
 	pop_f(stack, line_number);
 	(*stack)->n = sum;
 }
+
+/**
+ * pchar_f - function that print the top in the satck
+ * @stack: is a double list reversed
+ * @line_number: line of file
+ * Return: top of stack
+ */
+
+void pchar_f(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+
+	if (*stack == NULL)
+	{
+		dprintf(STDERR_FILENO, "L%d: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (temp->n < 0 || temp->n > 255)
+	{
+		dprintf(STDERR_FILENO, "L%d: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(void)line_number;
+	printf("%c\n", temp->n);
+}

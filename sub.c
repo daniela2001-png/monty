ace9c7d15e7a3b35cea0341ace62d1d66fd688cf
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-05-14 19:15:53 -0500

    [update]: add function modulo

--- sub.c
+++ sub.c
@@ -93,3 +93,39 @@ void mul_f(stack_t **stack, unsigned int line_number)
 	pop_f(stack, line_number);
 	(*stack)->n = sum;
 }
+
+/**
+* module - function that put the module
+* @head: double list reversed
+* @line_number: line of file
+* Return:
+*/
+void module(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+	int sum, count = 0, n;
+
+	if ((*stack)->n == 0)
+	{
+		dprintf(STDERR_FILENO, "L%u: division by zero\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+	tmp = *stack;
+	while (tmp)
+	{
+		tmp = tmp->next;
+		count++;
+	}
+	if (count < 2)
+	{
+		dprintf(STDERR_FILENO, "L%u: can't mod, stack too short\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+	sum = (*stack)->next->n;
+	n = (*stack)->n;
+	sum = sum % n;
+	pop_f(stack, line_number);
+	(*stack)->n = sum;
+}
